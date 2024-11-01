df8da4f0b5e474a6fd55eb823296ad27f02861aa
Georgi <hristovgeorgivalentinov@gmail.com>
2024-11-01 12:28:26 +0200

    add employee response dto class

---
+++ src/main/java/com/sirmaacademy/employeemanagementsystemrestapi/model/response/EmployeeEntityResponse.java
@@ -0,0 +1,48 @@
+package com.sirmaacademy.employeemanagementsystemrestapi.model.response;
+
+public class EmployeeEntityResponse {
+
+    private String firstName;
+    private String lastName;
+    private String department;
+    private String position;
+
+    public EmployeeEntityResponse(String firstName, String lastName, String department, String position) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.department = department;
+        this.position = position;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+}
