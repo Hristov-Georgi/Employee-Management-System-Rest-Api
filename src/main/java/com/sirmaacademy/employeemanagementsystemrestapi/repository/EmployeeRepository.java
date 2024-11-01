fc04f242d13058cdb44f5624a916b8b1e71c7121
Georgi <hristovgeorgivalentinov@gmail.com>
2024-11-01 12:23:12 +0200

    add findAllByUsername and findAllWithStatusActive repository methods

--- src/main/java/com/sirmaacademy/employeemanagementsystemrestapi/repository/EmployeeRepository.java
+++ src/main/java/com/sirmaacademy/employeemanagementsystemrestapi/repository/EmployeeRepository.java
@@ -2,10 +2,19 @@ package com.sirmaacademy.employeemanagementsystemrestapi.repository;
 
 import com.sirmaacademy.employeemanagementsystemrestapi.model.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, String> {
 
     boolean existsByPersonalIdNumber(Integer personalIdNumber);
+
+    Optional<List<Employee>> findAllByFirstName(String firstName);
+
+    @Query("SELECT e FROM Employee e WHERE e.status = 'ACTIVE'") //TODO: check query
+    Optional<List<Employee>> findAllWithStatusActive();
 }
