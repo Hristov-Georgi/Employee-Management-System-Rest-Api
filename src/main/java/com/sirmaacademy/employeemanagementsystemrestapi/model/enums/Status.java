b8f26d5f239b50e78656679aab1e96629ba2b028
Georgi <hristovgeorgivalentinov@gmail.com>
2024-11-01 12:39:07 +0200

    documented Status enum class

--- src/main/java/com/sirmaacademy/employeemanagementsystemrestapi/model/enums/Status.java
+++ src/main/java/com/sirmaacademy/employeemanagementsystemrestapi/model/enums/Status.java
@@ -2,8 +2,8 @@ package com.sirmaacademy.employeemanagementsystemrestapi.model.enums;
 
 /**
  * This enumeration class is used to show employees status in the company.
- * If it is current employee status = ACTIVE,
- * else if it is not working anymore for the company status = FIRED;
+ * If it is current employee, status = ACTIVE,
+ * else if it is not working anymore for the company status = FIRED or LEFT;
  */
 
 public enum Status {
