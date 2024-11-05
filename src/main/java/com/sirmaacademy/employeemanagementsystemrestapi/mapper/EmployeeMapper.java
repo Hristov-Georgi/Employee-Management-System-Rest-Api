d129cb399bf6a3a715c10c68122ed1fdd3fc9564
Georgi <hristovgeorgivalentinov@gmail.com>
2024-11-05 12:39:37 +0200

    fix employee to EmployeeEntityResponse mapping

--- src/main/java/com/sirmaacademy/employeemanagementsystemrestapi/mapper/EmployeeMapper.java
+++ src/main/java/com/sirmaacademy/employeemanagementsystemrestapi/mapper/EmployeeMapper.java
@@ -3,6 +3,7 @@ package com.sirmaacademy.employeemanagementsystemrestapi.mapper;
 import com.sirmaacademy.employeemanagementsystemrestapi.model.entity.Employee;
 import com.sirmaacademy.employeemanagementsystemrestapi.model.response.EmployeeEntityResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
@@ -14,5 +15,8 @@ public interface EmployeeMapper {
 
     List<EmployeeEntityResponse> employeeToEmployeeResponseList(List<Employee> employees);
 
+    @Mapping(target = "departmentName", source = "department.name")
+    @Mapping(target = "position", source = "position.name")
     EmployeeEntityResponse employeeToEmployeeEntityResponse(Employee employee);
+
 }
